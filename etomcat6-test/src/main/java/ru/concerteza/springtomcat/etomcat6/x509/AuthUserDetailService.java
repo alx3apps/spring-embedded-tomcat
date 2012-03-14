b6c17b21575c93c1145732544b9221c6732963df
alexey <kalexey@concerteza.ru>
2012-03-15 00:36:49 +0400

    x509 test fix

--- etomcat6-test/src/main/java/ru/concerteza/springtomcat/etomcat6/x509/AuthUserDetailService.java
+++ etomcat6-test/src/main/java/ru/concerteza/springtomcat/etomcat6/x509/AuthUserDetailService.java
@@ -1,11 +1,9 @@
 package ru.concerteza.springtomcat.etomcat6.x509;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -15,14 +13,14 @@ import java.util.Arrays;
  * Date: 3/4/12
  */
 
-@Service("authUserDetailService")
-public class AuthUserDetailService implements AuthenticationUserDetailsService {
+@Service("authUserDetailsService")
+public class AuthUserDetailService implements UserDetailsService {
     @Override
-    public UserDetails loadUserDetails(Authentication token) throws UsernameNotFoundException {
-        if("etomcat_client".equals(token.getName())) {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException {
+        if("etomcat_client".equals(username)) {
             return new User("etomcat_client", "", true, true, true, true, Arrays.asList(new Role("ROLE_ETOMCAT_CLIENT")));
         }
-        throw new UsernameNotFoundException(token.getName());
+        throw new UsernameNotFoundException(username);
     }
 
     private class Role implements GrantedAuthority {

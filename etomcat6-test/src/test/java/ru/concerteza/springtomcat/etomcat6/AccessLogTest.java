7bd683400f110344e61853ffac5d9ce923af5ff3
alexey <kalexey@concerteza.ru>
2014-12-03 23:46:49 +0000

    spring version update

--- etomcat6-test/src/test/java/ru/concerteza/springtomcat/etomcat6/AccessLogTest.java
+++ etomcat6-test/src/test/java/ru/concerteza/springtomcat/etomcat6/AccessLogTest.java
@@ -6,27 +6,9 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionVoter;
-import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
-import org.springframework.security.access.vote.AffirmativeBased;
-import org.springframework.security.access.vote.RoleVoter;
-import org.springframework.security.authentication.AnonymousAuthenticationProvider;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.web.access.expression.ExpressionBasedFilterInvocationSecurityMetadataSource;
-import org.springframework.security.web.access.expression.WebExpressionVoter;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
-import org.springframework.security.web.util.AntPathRequestMatcher;
-import org.springframework.security.web.util.RequestMatcher;
 import ru.concerteza.springtomcat.components.access.AccessEvent;
 import ru.concerteza.springtomcat.etomcat6.components.TestLogConsumer;
 import ru.concerteza.springtomcat.etomcat6.components.TestServlet;
-import ru.concerteza.springtomcat.etomcat6.context.EmbeddedAnnotationSpringContext;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
